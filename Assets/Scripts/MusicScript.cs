d6383cec6eab599c610fe07f8aa2b8c027b6c5af
CristianCerutti <71840592+CristianCerutti@users.noreply.github.com>
2021-03-04 09:49:21 +0100

    Add files via upload

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MusicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+    private float musicVolume = 1f;
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
